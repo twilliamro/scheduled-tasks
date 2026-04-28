74f8fc5a4eeb39f2376b6a443fae423ae984cbe7
twilliamro <66513087+twilliamro@users.noreply.github.com>
2026-04-28 16:15:29 -0300

    Update email credentials and SMTP server details

--- main.py
+++ main.py
@@ -13,14 +13,15 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("python100xtest@gmail.com")
+MY_PASSWORD = os.environ.get("pggh tksa qccp btcn")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+birthdays_dict = {(data_row["month"], data_row["day"])                  
+        : data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
@@ -28,7 +29,7 @@ if today_tuple in birthdays_dict:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
